66fea256bdc48ad503c6e374b7100f9f1a8c252a
Sandor Dargo <sandor.dargo@gmail.com>
2016-05-31 22:13:39 +0200

    refactor: Rename tests

--- cpp/GildedRoseUnitTests.cc
+++ cpp/GildedRoseUnitTests.cc
@@ -2,7 +2,7 @@
 
 #include "GildedRose.h"
 
-TEST(GildedRoseTest, Foo) {
+TEST(GildedRoseTest, NomralDecreaseForRandomItem) {
     vector<Item> items;
     items.push_back(Item("Foo", 10, 20));
     GildedRose app(items);
@@ -14,7 +14,7 @@ TEST(GildedRoseTest, Foo) {
     EXPECT_EQ(19, app.items[0].quality);
 }
 
-TEST(GildedRoseTest, SellinDatePast) {
+TEST(GildedRoseTest, NegativeSellinValue) {
     vector<Item> items;
     items.push_back(Item("Foo", -1, 20));
     GildedRose app(items);
@@ -26,7 +26,7 @@ TEST(GildedRoseTest, SellinDatePast) {
     EXPECT_EQ(18, app.items[0].quality);
 }
 
-TEST(GildedRoseTest, QualityNotNegative) {
+TEST(GildedRoseTest, ZeroQualityShouldNotGoNegative) {
     vector<Item> items;
     items.push_back(Item("Foo", 10, 0));
     GildedRose app(items);
@@ -38,7 +38,7 @@ TEST(GildedRoseTest, QualityNotNegative) {
     EXPECT_EQ(0, app.items[0].quality);
 }
 
-TEST(GildedRoseTest, AgedBrie) {
+TEST(GildedRoseTest, AgedBrieIncreasesQuality) {
     vector<Item> items;
     items.push_back(Item("Aged Brie", 10, 20));
     GildedRose app(items);
@@ -50,7 +50,7 @@ TEST(GildedRoseTest, AgedBrie) {
     EXPECT_EQ(21, app.items[0].quality);
 }
 
-TEST(GildedRoseTest, AgedBrieAfterSellinDate) {
+TEST(GildedRoseTest, AgedBrieDoubleQualityIncreaseAfterNegativeSellinDate) {
     vector<Item> items;
     items.push_back(Item("Aged Brie", -1, 20));
     GildedRose app(items);
@@ -88,7 +88,7 @@ TEST(GildedRoseTest, QualityCappedAtFifty) {
     EXPECT_EQ(50, app.items[0].quality);
 }
 
-TEST(GildedRoseTest, Sulfuras) {
+TEST(GildedRoseTest, SulfurasDoesNotChange) {
     vector<Item> items;
     items.push_back(Item("Sulfuras, Hand of Ragnaros", 10, 20));
     GildedRose app(items);
@@ -99,7 +99,7 @@ TEST(GildedRoseTest, Sulfuras) {
     EXPECT_EQ(20, app.items[0].quality);
 }
 
-TEST(GildedRoseTest, PassesMoreThan10Days) {
+TEST(GildedRoseTest, BackstagePassIncreaseQualityBy1MoreThan10Days) {
     vector<Item> items;
     items.push_back(Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
     GildedRose app(items);
@@ -110,7 +110,7 @@ TEST(GildedRoseTest, PassesMoreThan10Days) {
     EXPECT_EQ(21, app.items[0].quality);
 }
 
-TEST(GildedRoseTest, Passes10Days) {
+TEST(GildedRoseTest, BackstagePassIncreaseQualityBy2Before10Days) {
     vector<Item> items;
     items.push_back(Item("Backstage passes to a TAFKAL80ETC concert", 10, 20));
     GildedRose app(items);
@@ -121,7 +121,7 @@ TEST(GildedRoseTest, Passes10Days) {
     EXPECT_EQ(22, app.items[0].quality);
 }
 
-TEST(GildedRoseTest, Passes5Days) {
+TEST(GildedRoseTest, BackstagePassIncreaseQualityBy3LessThan5Days) {
     vector<Item> items;
     items.push_back(Item("Backstage passes to a TAFKAL80ETC concert", 5, 20));
     GildedRose app(items);
@@ -132,7 +132,7 @@ TEST(GildedRoseTest, Passes5Days) {
     EXPECT_EQ(23, app.items[0].quality);
 }
 
-TEST(GildedRoseTest, Passes0Day) {
+TEST(GildedRoseTest, BackstagePassQualityGoesZeroWithNegativeSellinDate) {
     vector<Item> items;
     items.push_back(Item("Backstage passes to a TAFKAL80ETC concert", 0, 20));
     GildedRose app(items);
