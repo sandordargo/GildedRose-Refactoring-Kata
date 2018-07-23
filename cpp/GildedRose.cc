ac069ef55e1a5cd5a0c512faad6b104ecc4bbc30
Sandor Dargo <sandordargo@users.noreply.github.com>
2018-07-23 16:50:10 +0200

    Step 2: Implement the quality and sellIn behaviour for non-special items

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -5,60 +5,71 @@ GildedRose::GildedRose(vector<Item> & items) : items(items)
     
 void GildedRose::processItem(Item & item)
 {
-    if(item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"){
-        if(item.quality > 0){
-            if(item.name != "Sulfuras, Hand of Ragnaros"){
-                item.quality = item.quality - 1;
-            }
-        }
+	if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" && item.name != "Sulfuras, Hand of Ragnaros") {
+		item.sellIn--;
+		if (item.quality > 0) {
+			item.quality--;
+			if (item.quality > 0 && item.sellIn < 0) {
+				item.quality--;
+			}
+		}
 
-    }
-    else{
-        if(item.quality < 50){
-            item.quality = item.quality + 1;
-            if(item.name == "Backstage passes to a TAFKAL80ETC concert"){
-                if(item.sellIn < 11){
-                    if(item.quality < 50){
-                        item.quality = item.quality + 1;
-                    }
-                }
+	} else {
+		if(item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"){
+			if(item.quality > 0){
+				if(item.name != "Sulfuras, Hand of Ragnaros"){
+					item.quality = item.quality - 1;
+				}
+			}
 
-                if(item.sellIn < 6){
-                    if(item.quality < 50){
-                        item.quality = item.quality + 1;
-                    }
-                }
+		}
+		else{
+			if(item.quality < 50){
+				item.quality = item.quality + 1;
+				if(item.name == "Backstage passes to a TAFKAL80ETC concert"){
+					if(item.sellIn < 11){
+						if(item.quality < 50){
+							item.quality = item.quality + 1;
+						}
+					}
 
-            }
+					if(item.sellIn < 6){
+						if(item.quality < 50){
+							item.quality = item.quality + 1;
+						}
+					}
 
-        }
+				}
 
-    }
+			}
 
-    if(item.name != "Sulfuras, Hand of Ragnaros"){
-        item.sellIn = item.sellIn - 1;
-    }
-    if(item.sellIn < 0){
-        if(item.name != "Aged Brie"){
-            if(item.name != "Backstage passes to a TAFKAL80ETC concert"){
-                if(item.quality > 0){
-                    if(item.name != "Sulfuras, Hand of Ragnaros"){
-                        item.quality = item.quality - 1;
-                    }
-                }
+		}
 
-            }
-            else{
-                item.quality = item.quality - item.quality;
-            }
-        }
-        else{
-            if(item.quality < 50){
-                item.quality = item.quality + 1;
-            }
-        }
+		if(item.name != "Sulfuras, Hand of Ragnaros"){
+			item.sellIn = item.sellIn - 1;
+		}
+		if(item.sellIn < 0){
+			if(item.name != "Aged Brie"){
+				if(item.name != "Backstage passes to a TAFKAL80ETC concert"){
+					if(item.quality > 0){
+						if(item.name != "Sulfuras, Hand of Ragnaros"){
+							item.quality = item.quality - 1;
+						}
+					}
 
-    }
+				}
+				else{
+					item.quality = item.quality - item.quality;
+				}
+			}
+			else{
+				if(item.quality < 50){
+					item.quality = item.quality + 1;
+				}
+			}
+
+		}
+	}
 
 }
 
