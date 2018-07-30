fcfa2ca3826f865c03bfea33808d6701c64add28
Sandor Dargo <sandordargo@users.noreply.github.com>
2018-07-30 08:57:32 +0200

    Step 8: Remove the branching from Gildedrose::processItem

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -5,79 +5,8 @@ GildedRose::GildedRose(vector<Item> & items) : items(items)
     
 void GildedRose::processItem(Item & item)
 {
-	if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" && item.name != "Sulfuras, Hand of Ragnaros") {
-		item.updateSellIn();
-		item.updateQuality();
-
-	} else if (item.name == "Sulfuras, Hand of Ragnaros") {
-		item.updateSellIn();
-		item.updateQuality();
-	} else if (item.name == "Aged Brie") {
-		item.updateSellIn();
-		item.updateQuality();
-	} else if (item.name.substr(0, 16) == "Backstage passes") {
-		item.updateSellIn();
-		item.updateQuality();
-	} else if (item.name.substr(0, 8) == "Conjured") {
-		item.updateSellIn();
-		item.updateQuality();
-	} else {
-		if(item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"){
-			if(item.quality > 0){
-				if(item.name != "Sulfuras, Hand of Ragnaros"){
-					item.quality = item.quality - 1;
-				}
-			}
-
-		}
-		else{
-			if(item.quality < 50){
-				item.quality = item.quality + 1;
-				if(item.name == "Backstage passes to a TAFKAL80ETC concert"){
-					if(item.sellIn < 11){
-						if(item.quality < 50){
-							item.quality = item.quality + 1;
-						}
-					}
-
-					if(item.sellIn < 6){
-						if(item.quality < 50){
-							item.quality = item.quality + 1;
-						}
-					}
-
-				}
-
-			}
-
-		}
-
-		if(item.name != "Sulfuras, Hand of Ragnaros"){
-			item.sellIn = item.sellIn - 1;
-		}
-		if(item.sellIn < 0){
-			if(item.name != "Aged Brie"){
-				if(item.name != "Backstage passes to a TAFKAL80ETC concert"){
-					if(item.quality > 0){
-						if(item.name != "Sulfuras, Hand of Ragnaros"){
-							item.quality = item.quality - 1;
-						}
-					}
-
-				}
-				else{
-					item.quality = item.quality - item.quality;
-				}
-			}
-			else{
-				if(item.quality < 50){
-					item.quality = item.quality + 1;
-				}
-			}
-
-		}
-	}
-
+	item.updateSellIn();
+	item.updateQuality();
 }
 
 void GildedRose::updateQuality() 
